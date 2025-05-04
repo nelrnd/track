f0018bbedf7ed78bbd575d42606d131e16920df8
Nel <nel.renaudin@gmail.com>
2025-05-04 13:29:14 +0200

    fix lint error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import Calendar from "@/components/calendar"
 import HabitList from "@/components/habit-list"
 import { fetchHabits, fetchTracks } from "@/lib/data"
-import { createYear } from "@/lib/utils"
-
-const year = createYear(2025)
 
 export default async function Home() {
   const [habits, tracks] = await Promise.all([fetchHabits(), fetchTracks()])
